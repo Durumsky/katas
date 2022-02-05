a5f41b8b963a04815c1d5c1ccbb31c8940d9b93c
Jones Durumsky <jonesdurumsky@gmail.com>
2022-02-05 10:24:50 +0100

    solution to https://www.codewars.com/kata/513e08acc600c94f01000001/javascript

--- katas.js
+++ katas.js
@@ -207,3 +207,45 @@ function anagrams(word, words) {
 
   return anagramArr;
 }
+
+//https://www.codewars.com/kata/513e08acc600c94f01000001
+//RGB to HEX convertion
+
+function rgb(r, g, b) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let hex = [];
+  r < 0 ? (r = 0) : r;
+  g < 0 ? (g = 0) : g;
+  b < 0 ? (b = 0) : b;
+  r > 255 ? (r = 255) : r;
+  g > 255 ? (g = 255) : g;
+  b > 255 ? (b = 255) : b;
+
+  hex.push(Math.floor(r / 16));
+  hex.push((r / 16 - Math.floor(r / 16)) * 16);
+  hex.push(Math.floor(g / 16));
+  hex.push((g / 16 - Math.floor(g / 16)) * 16);
+  hex.push(Math.floor(b / 16));
+  hex.push((b / 16 - Math.floor(b / 16)) * 16);
+  hex = hex.map((n) => {
+    if (n < 10) {
+      return n;
+    } else {
+      return String.fromCharCode(alphabet.charCodeAt(n - 10)).toUpperCase();
+    }
+  });
+
+  return hex.join('');
+}
+
+//way smarter:
+
+function rgb(r, g, b){
+  return toHex(r)+toHex(g)+toHex(b);
+}
+
+function toHex(d) {
+    if(d < 0 ) {return "00";}
+    if(d > 255 ) {return "FF";}
+    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
